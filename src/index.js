6edd6a3c40bf555af32dc09eaf71e926fbc53c08
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-30 22:32:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+   try {
+    // Use fs.promises.readFile to read the existing content of the file
+    const existingContent = await fs.promises.readFile(fileName, "utf-8");
+
+    // Combine the existing content with the new content to be added
+    const updatedContent = `${existingContent.trim()}, ${fileContent.trim()}`;
+
+    // Use fs.promises.writeFile to update the content of the file
+    await fs.promises.writeFile(fileName, updatedContent, "utf-8");
+  } catch (error) {
+    // Handle any errors that might occur during file update
+    console.error("Error updating the file:", error.message);
+    throw error;
+  }
   
 };
 
